9fd111271c9aacb2e37c18bf0e483f9333f8f4f5
Chris Dragan <chris@chris.dragan.name>
2017-06-28 20:35:03 +0100

    core: add comment/remark

--- core/kos_object_alloc.h
+++ core/kos_object_alloc.h
@@ -42,6 +42,9 @@ enum _KOS_AREA_ELEM_SIZE {
     KOS_AREA_128  = 7
 };
 
+/* TODO allocate array, buffer in 32-bit builds from KOS_AREA_8, but limit
+ * them to be allocated in even-index cells only. */
+
 #define _KOS_POT_FROM_TYPE(type) ((OBJ_ ## type & 1)   == 0   ? KOS_AREA_8   : \
                                   sizeof(KOS_ ## type) <= 16  ? KOS_AREA_16  : \
                                   sizeof(KOS_ ## type) <= 32  ? KOS_AREA_32  : \
