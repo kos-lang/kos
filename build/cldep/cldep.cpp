7c03a946ae2e0e425071c474cea6797009f25c0d
Chris Dragan <chris@chris.dragan.name>
2017-07-26 21:12:41 +0100

    cldep: ignore more warnings in windows.h in VS2017

--- build/cldep/cldep.cpp
+++ build/cldep/cldep.cpp
@@ -22,7 +22,7 @@
 
 #define WIN32_LEAN_AND_MEAN
 #pragma warning( push )
-#pragma warning( disable : 4255 4668 )
+#pragma warning( disable : 4255 4365 4668 4774 4987 )
 #include <windows.h>
 #pragma warning( pop )
 
