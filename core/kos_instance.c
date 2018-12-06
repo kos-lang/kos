2fec4686af6f8751de5f6272e89da290f7141c46
Chris Dragan <chris@chris.dragan.name>
2018-12-06 21:00:23 +0000

    core: fix compilation with GCC and MSVC

--- core/kos_instance.c
+++ core/kos_instance.c
@@ -1038,7 +1038,7 @@ int KOS_push_locals(KOS_CONTEXT ctx, int* push_status, int num_entries, ...)
             *(ptr++) = (KOS_OBJ_ID *)va_arg(args, KOS_OBJ_ID *);
         while (ptr < end);
 
-        OBJPTR(LOCAL_REFS, local_refs)->header.num_tracked = num_tracked + num_entries;
+        OBJPTR(LOCAL_REFS, local_refs)->header.num_tracked = num_tracked + (unsigned)num_entries;
     }
     else {
         KOS_OBJ_ID **helper_refs = &ctx->helper_refs[0];
@@ -1070,7 +1070,7 @@ int KOS_push_locals(KOS_CONTEXT ctx, int* push_status, int num_entries, ...)
                 *(ptr++) = *(helper_refs++);
             while (ptr < end);
 
-            OBJPTR(LOCAL_REFS, local_refs)->header.num_tracked = num_tracked + num_entries;
+            OBJPTR(LOCAL_REFS, local_refs)->header.num_tracked = num_tracked + (unsigned)num_entries;
         }
 
         ctx->helper_ref_count = 0;
@@ -1108,12 +1108,12 @@ void KOS_pop_locals(KOS_CONTEXT ctx, int push_status)
             assert(num_tracked > 0);
 
             assert(OBJPTR(LOCAL_REFS, local_refs)->header.prev_scope == KOS_LOOK_FURTHER ||
-                   OBJPTR(LOCAL_REFS, local_refs)->header.prev_scope + 1 < num_tracked);
+                   OBJPTR(LOCAL_REFS, local_refs)->header.prev_scope + 1U < num_tracked);
         }
 
         assert(num_tracked >= (unsigned)push_status);
 
-        OBJPTR(LOCAL_REFS, local_refs)->header.num_tracked = num_tracked - push_status;
+        OBJPTR(LOCAL_REFS, local_refs)->header.num_tracked = num_tracked - (unsigned)push_status;
     }
 }
 
