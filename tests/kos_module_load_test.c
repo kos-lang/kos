60fc6ce955c073a55e01a83462684735ec1e16b8
Chris Dragan <chris@chris.dragan.name>
2019-03-09 22:00:06 +0000

    core: add test for KOS_load_module_from_memory

---
+++ tests/kos_module_load_test.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2014-2019 Chris Dragan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "../inc/kos_error.h"
+#include "../inc/kos_instance.h"
+#include "../inc/kos_module.h"
+#include <stdio.h>
+
+#define TEST(test) do { if (!(test)) { printf("Failed: line %d: %s\n", __LINE__, #test); return 1; } } while (0)
+#define TEST_EXCEPTION() do { TEST(KOS_is_exception_pending(ctx)); KOS_clear_exception(ctx); } while (0)
+#define TEST_NO_EXCEPTION() TEST( ! KOS_is_exception_pending(ctx))
+
+int main(void)
+{
+    KOS_INSTANCE inst;
+    KOS_CONTEXT  ctx;
+
+    TEST(KOS_instance_init(&inst, KOS_INST_MANUAL_GC, &ctx) == KOS_SUCCESS);
+
+    /************************************************************************/
+    {
+        static const char module[] = "0";
+        TEST(KOS_load_module_from_memory(ctx, "base.kos", module, sizeof(module)) != KOS_SUCCESS);
+    }
+
+    KOS_instance_destroy(&inst);
+
+    return 0;
+}
