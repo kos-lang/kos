57da0d880eed76a7dc9939e66cc8290778894f71
Chris Dragan <chris@chris.dragan.name>
2020-01-26 12:43:19 +0000

    core: use bit scan in marking
    Use bit scan in algorithm for skipping white-marked objects.

--- core/kos_heap.c
+++ core/kos_heap.c
@@ -40,6 +40,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* For _BitScanForward() */
+#ifdef _MSC_VER
+#   pragma warning( push )
+#   pragma warning( disable : 4255 ) /* '__slwpcb': no function prototype given: converting '()' to '(void)' */
+#   pragma warning( disable : 4668 ) /* '__cplusplus' is not defined as a preprocessor macro */
+#   include <intrin.h>
+#   pragma warning( pop )
+#endif
+
 #if 0
 #define gc_trace(x) printf x
 #else
@@ -1166,6 +1175,32 @@ static void advance_marking(struct KOS_MARK_LOC_S *mark_loc,
     mark_loc->mask_idx =  mask_idx & 0x1FU;
 }
 
+static int find_first_set(uint32_t value)
+{
+#if (defined(__GNUC__) && ((__GNUC__ > 3) || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4))) || \
+    (defined(__clang__) && __has_builtin(__builtin_ctz))
+    return __builtin_ctz(value);
+#elif defined(_MSC_VER)
+    unsigned long bit;
+
+    _BitScanForward(&bit, value);
+
+    return (int)bit;
+#else
+    int bit;
+
+    static const signed char de_brujin_bit_pos[32] =
+    {
+        0,   1, 28 , 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17,  4, 8,
+        31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18,  6, 11,  5, 10, 9
+    };
+
+    bit = de_brujin_bit_pos[((value & -value) * 0x077CB531U) >> 27];
+
+    return bit;
+#endif
+}
+
 static uint32_t skip_white(KOS_SLOT             **out_ptr,
                            KOS_SLOT              *end,
                            struct KOS_MARK_LOC_S *mark_loc)
@@ -1173,6 +1208,8 @@ static uint32_t skip_white(KOS_SLOT             **out_ptr,
     KOS_SLOT *ptr;
     uint32_t  value    = KOS_atomic_read_relaxed_u32(*mark_loc->bitmap);
     uint32_t  mask_idx = mark_loc->mask_idx;
+    int       slot_idx;
+    int       bit_idx;
 
     value >>= mask_idx;
 
@@ -1198,34 +1235,16 @@ static uint32_t skip_white(KOS_SLOT             **out_ptr,
         } while ( ! value);
     }
 
-    /* TODO use bit scan */
-    if ( ! (value & 0xFFFFU)) {
-        ptr      +=  8U;
-        mask_idx +=  16U;
-        value    >>= 16;
-    }
+    slot_idx = find_first_set(value) >> 1;
+    bit_idx  = slot_idx * 2;
 
-    if ( ! (value & 0xFFU)) {
-        ptr      +=  4U;
-        mask_idx +=  8U;
-        value    >>= 8;
-    }
-
-    if ( ! (value & 0xFU)) {
-        ptr      +=  2U;
-        mask_idx +=  4U;
-        value    >>= 4;
-    }
-
-    if ( ! (value & 3U)) {
-        ptr      +=  1U;
-        mask_idx +=  2U;
-        value    >>= 2;
-    }
-
-    assert(value & 3U);
+    mask_idx +=  bit_idx;
+    ptr      +=  slot_idx;
+    value    >>= bit_idx;
 
     value &= 3U;
+
+    assert(value);
 
     mark_loc->mask_idx = mask_idx;
 
