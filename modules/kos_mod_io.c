54bc3d6c7e2a8d27f36b91f5eedb9a418f8f1a20
Chris Dragan <chris@chris.dragan.name>
2021-03-06 21:03:15 +0000

    io: remove debug prints

--- modules/kos_mod_io.c
+++ modules/kos_mod_io.c
@@ -244,10 +244,7 @@ static FILE *to_file(KOS_CONTEXT ctx, HANDLE *handle, const char *mode)
 
     KOS_suspend_context(ctx);
 
-printf("handle with mode %s -> detected %s\n", mode, strcmp(mode, "rb") ? "write" : "read");
-printf("    rdwr %d, wr %d, rd %d\n", _O_RDWR, _O_WRONLY, _O_RDONLY);
     fd = _open_osfhandle((intptr_t)*handle, strcmp(mode, "rb") ? _O_WRONLY : _O_RDONLY);
-printf("opened handle %d with flags %u mode %s\n", fd, strcmp(mode, "rb") ? (int)_O_WRONLY : (int)_O_RDONLY, mode);
 
     if (fd == -1)
         /* This is not correct, but unlikely to happen */
