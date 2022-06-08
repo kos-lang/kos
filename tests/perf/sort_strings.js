a026372c43e8ebf2de40436942e613e8344484f8
Chris Dragan <chris@chris.dragan.name>
2022-06-08 19:12:54 +0100

    tests: improve sort_strings perf test

--- tests/perf/sort_strings.js
+++ tests/perf/sort_strings.js
@@ -12,16 +12,20 @@ for (let i = 0; i < 100000; i++) {
     str_codes.length = 0;
 
     const r64 = rand_integer(0, 0x7FFFFFFFFFFFFFFF);
-    const r32 = [ (r64 / 0x100000000) & 0xFFFFFFFF, r64 & 0xFFFFFFFF ];
+    var   r1  = (r64 / 0x100000000) & 0xFFFFFFFF;
+    var   r0  = r64 & 0xFFFFFFFF;
 
-    for (let x = 0; x < 2; x++) {
-        let r = r32[x];
-
-        while (r) {
-            str_codes.push((r & 0x3F) + 0x20);
-            r >>= 7;
-        }
+    for (let j = 0; j < 4; j++) {
+        str_codes[j] = (r1 & 0x3F) + 0x20;
+        r1 = r1 >> 7;
     }
+
+    for (let j = 4; j < 8; j++) {
+        str_codes[j] = (r0 & 0x3F) + 0x20;
+        r0 = r0 >> 7;
+    }
+
+    str_codes[8] = (((r0 << 4) + r1) & 0x3F) + 0x20;
 
     a.push(String.fromCharCode.apply(null, str_codes));
 }
